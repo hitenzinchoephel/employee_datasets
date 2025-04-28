aaa1462fcefa8210b4b4e73dc9fcb1f6ce238697
Tenzin Choephel <tenzinchoephel2050@gmail.com>
2025-04-28 11:45:09 -0400

    Add files via upload
    List of business problems

---
+++ business problems.sql
@@ -0,0 +1,25 @@
+	    -- Q.1 Find the top 3 highest paid employees in each department 
+					
+		-- Q.2 Find the average salary of employees hired in the past 5 years
+				
+		-- Q.3 Find the employees whose salary is less than the average salary of employees hired in the last 5 yrs
+		
+		-- Q.4 Find the employees whose salary is greater or equal than the average salary of employees hired in the last 5 yrs
+		
+		-- Q.5 Find the higest salary employees 
+				
+		-- Q.6 Find the 2nd highest salary of the employee 
+		
+		-- Q.7 Find the nth highest salary using cte 
+	
+		-- Q.8 Find the first an last name from full name column( name )
+				
+		-- Q.9 Find the percentage of department salary
+		
+		-- Q.10 Find the max and min salary of employee
+
+		-- Q.11 How many employees joined in each year?
+
+		
+
+		
