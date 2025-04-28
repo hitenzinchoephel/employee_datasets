f86bd563429b785a612d44263990d3650b7b2a5e
Tenzin Choephel <tenzinchoephel2050@gmail.com>
2025-04-28 14:38:34 -0400

    Update employee_biz_solutions.sql

--- employee_biz_solutions.sql
+++ employee_biz_solutions.sql
@@ -32,180 +32,100 @@
    ----------------------------------------------------------------------------
    ----------------------------------------------------------------------------
 
-	--=================  START ===========
+	-- START --   
    
-	-- Q.1 Find the top 3 highest paid employees in each department 
+-- Q.1 Find the top 3 highest paid employees in each department 
 
-	WITH
-		CTE AS (
-			SELECT
-				EMP_ID,
-				NAME,
-				DEPARTMENT,
-				SALARY,
-				DENSE_RANK() OVER (
-					PARTITION BY
-						DEPARTMENT
-					ORDER BY
-						SALARY DESC
-				) AS RNK
-			FROM
-				EMPLOYEE
-		)
-	SELECT
-		*
-	FROM
-		CTE
-	WHERE
-		RNK <= 3
+
+    With cte as (
+	select
+		emp_id,
+		name,
+		depart,
+		salary,
+		dense_rank() over (partition by department order by salary desc) AS RNK
+        from employee)
+   select * from cte where rnk<=3;
+
+--  Q.2 Find the average salary of employees hired in the past 5 years
+
+	select
+             round(avg(salary), 2) as average_salary
+         from employee
+         where joining_date >= cast(current_date - interval '5 years' as date);
+
+--  Q.3 Find the employees whose salary is less than the average salary of employees hired in the last 5 yrs
+
+       select *
+       from employee
+       where salary < (select round(avg(salary), 2) as average_salary from employee
+                        where joining_date >= cast(current_date - interval '5 years' as date)
+			);
+--  Q.4 Find the employees whose salary is greater or equal than the average salary of employees hired in the last 5 yrs
 		
-	-- Q.2 Find the average salary of employees hired in the past 5 years
+      select * from employee
+		where salary >= ( select round(avg(salary), 2) as average_salary from employee
+                                  where joining_date  >= cast(current_date - interval '5 years' as date)) ;
 
-	SELECT
-		ROUND(AVG(SALARY), 2) AS AVERAGE_SALARY
-	FROM
-		EMPLOYEE
-	WHERE
-		JOINING_DATE >= CAST(CURRENT_DATE - INTERVAL '5 years' AS DATE)
-		-- Answer is 83714.29 
+-- Q.5 Find the higest salary employees 
+
+	select * from employee where salary = ( select  max(salary) from employee )
+
+			
+-- Q.6 Find the 2nd highest salary of the employee 
+
+		select max(salary) from employee
+		      where salary <(select max(salary) from employee)
+			
+-- Q.7 Find the nth highest salary using cte  (here we wil find second )
+
+     with cte_salary_rank as (
+	select *,
+		  row_number() over (order by salary desc) as rn
+	from employee
+	)	
+     select * from cte_salary_rank where rn = 2
+
+
+-- Q.8 Find the first an last name from full name column( name )
+
+	select *,
+	   substring(name from 1 for position(' ' in name) - 1) as first_name,
+	   substring(name from position(' ' in name) + 1) as last_name
+	from employee
+
 		
-	-- Q.3 Find the employees whose salary is less than the average salary of employees hired in the last 5 yrs
+--  Q.9 Find the percentage of department salary 
 
-	SELECT
-		*
-	FROM
-		EMPLOYEE
-	WHERE
-		SALARY < (
-			SELECT
-				ROUND(AVG(SALARY), 2) AS AVERAGE_SALARY
-			FROM
-				EMPLOYEE
-			WHERE
-				JOINING_DATE >= CAST(CURRENT_DATE - INTERVAL '5 years' AS DATE)
-		)
-		
-	-- Q.4 Find the employees whose salary is greater or equal than the average salary of employees hired in the last 5 yrs
-	
-	SELECT
-		*
-	FROM
-		EMPLOYEE
-	WHERE
-		SALARY >= (
-			SELECT
-				ROUND(AVG(SALARY), 2) AS AVERAGE_SALARY
-			FROM
-				EMPLOYEE
-			WHERE
-				JOINING_DATE >= CAST(CURRENT_DATE - INTERVAL '5 years' AS DATE)
-		)
-	-- Q.5 Find the higest salary employees 
-
-	SELECT
-		*
-	FROM
-		EMPLOYEE
-	WHERE
-		SALARY = (
-			SELECT
-				MAX(SALARY)
-			FROM
-				EMPLOYEE
-		)
-		
-	-- Q.6 Find the 2nd highest salary of the employee 
+    select
+	department,
+	sum(salary) as total,
+	round(sum(salary) * 100.00 / (select sum(salary)from employee),2) as percentage
+    from employee
+    group by 1
+    order by total desc
 
 
-	SELECT
-		MAX(SALARY)
-	FROM
-		EMPLOYEE
-	WHERE
-		SALARY < (
-			SELECT
-				MAX(SALARY)
-			FROM
-				EMPLOYEE
-		)
-		
-	-- Q.7 Find the nth highest salary using cte 
+-- Q.10 Find the max and min salary of employee
+ 
+        select 
+           max(salary) AS max_salary, 
+            min(salary) AS min_salary
+        from employee
 
-	WITH
-		CTE_SALARY_RANK AS (
-			SELECT
-				*,
-				ROW_NUMBER() OVER (
-					ORDER BY
-						SALARY DESC
-				) AS RN
-			FROM
-				EMPLOYEE
-		)
-	SELECT
-		*
-	FROM
-		CTE_SALARY_RANK
-	WHERE
-		RN = 2
-		
-	-- Q.8 Find the first an last name from full name column( name )
-	
-	SELECT
-		*,
-		SUBSTRING(
-			NAME
-			FROM
-				1 FOR POSITION(' ' IN NAME) - 1
-		) AS FIRST_NAME,
-		SUBSTRING(
-			NAME
-			FROM
-				POSITION(' ' IN NAME) + 1
-		) AS LAST_NAME
-	FROM
-		EMPLOYEE
-	
-	-- Q.9 Find the percentage of department salary 
+  
+-- Q.11 How many employees joined in each year?
 
-	SELECT
-		DEPARTMENT,
-		SUM(SALARY) AS TOTAL,
-		ROUND(
-			SUM(SALARY) * 100.00 / (
-				SELECT
-					SUM(SALARY)
-				FROM
-					EMPLOYEE
-			),
-			2
-		) AS PERCENTAGE
-	FROM
-		EMPLOYEE
-	GROUP BY
-		1
-	ORDER BY
-		TOTAL DESC
+          select 
+              extract('year' from joining_date) as year,
+              count(*) as no_of_emp_joined 
+          from employee
+          group by year
+          order by year
 
+ -- Q.12 Find mean and median salary of employees
 
-	-- Q.10 Find the max and min salary of employee
-
-	SELECT 
-		   MAX(salary) AS max_salary, 
-		   MIN(salary) AS min_salary
-	FROM employee
-
-	-- Q.11 How many employees joined in each year?
-
-	SELECT 
-		EXTRACT('year' FROM joining_date) AS year,
-		COUNT(*) AS no_of_emp_joined 
-	FROM employee
-	GROUP BY year 
-	ORDER BY year
-
-	-- Q.12 Find mean and median salary of employees
-		-- mean salary --
+        -- mean salary --
 
 	select round(avg(salary),2) as average_salary from employee
 
@@ -214,7 +134,6 @@
 	select percentile_cont(0.5) within group(order by salary) as median_salary from employee
 
 
-
--- ================= END OF PROJECT ==========================
+        --  END OF PROJECT -- 
 
 	
